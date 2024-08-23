9b8144accb1fe23a3eef5de31a874841cf20e352
Andhre <mizandhre@gmail.com>
2024-08-22 23:52:10 -0500

    V 1.8

--- index.js
+++ index.js
@@ -27,17 +27,20 @@ app.use(express.json());
 conectarDB();
 
 //dominios permitidos ==> CORS
-const dominiosPermitidos = [process.env.FRONTEND_URL];
-const corsOptions = {
-	origin: function (origin, callback) {
-		if (dominiosPermitidos.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else {
-			callback(new Error('No permitido por CORS'));
-		}
-	},
-};
-app.use(cors(corsOptions));
+// const dominiosPermitidos = [process.env.FRONTEND_URL];
+// const corsOptions = {
+// 	origin: function (origin, callback) {
+// 		// if (dominiosPermitidos.indexOf(origin) !== -1) {
+// 		if (dominiosPermitidos.indexOf(origin) !== -1 || !origin) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error('No permitido por CORS'));
+// 		}
+// 	},
+// };
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // Crear las rutas API
 app.use('/api/usuarios', usuarioRoutes);
